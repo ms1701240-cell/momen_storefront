6588264af0009c2af98cc3920b0cf0e7ea18fe0a
ms1701240-cell <ms1701240@gmail.com>
2026-05-13 19:10:30 +0300

    added ads.txt and adx tags

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
- import './Bootstrap/bootstrap.min.css';
- import './Bootstrap/bootstrap.bundle.min.js';
- import './fontawome/all.min.css';
+ import './bootstrap/bootstrap.min.css';
+import './bootstrap/bootstrap.bundle.min.js';
+import './fontawome/all.min.css';
 
 
 createRoot(document.getElementById('root')).render(
